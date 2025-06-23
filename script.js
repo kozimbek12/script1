57cccc181c8201e73438aa9fbaaacc31c8248397
kozimbek12 <kozimbekng@gmail.com>
2025-06-23 17:02:04 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let userName = prompt('Ismingizni kiriting')
+let userAge = prompt('Yoshingizni kiriting')
+let userSurname = prompt('Familiyangizni kiring')
+let userFrom = prompt('Shaxringizni kiriting')
+let userfani = prompt('yoqtirgan faningizni kiriting')
+
+
+
+
+
+
+let user = {
+ name: userName,
+age: userAge,
+surname: userSurname,
+from: userFrom,
+yoqtirganfani:userfani,
+}
+
+console.log(user);
